913c39e49909a5a6a75683e46c5a05748f83ec1a
tia <mattiadmr@gmail.com>
2016-02-28 16:51:32 +0100

    Document can contain numbers as well

--- lib/lda-ruby/document/document.rb
+++ lib/lda-ruby/document/document.rb
@@ -32,7 +32,7 @@ module Lda
     end
 
     def tokenize(text)
-      clean_text = text.gsub(/[^a-zäöüß'-]+/i, ' ').gsub(/\s+/, ' ').downcase  # remove everything but letters and ' and leave only single spaces
+      clean_text = text.gsub(/[äöüß'-]+/i, ' ').gsub(/\s+/, ' ').downcase  # remove everything but letters and ' and leave only single spaces
       @tokens = handle(clean_text.split(' '))
       nil
     end
